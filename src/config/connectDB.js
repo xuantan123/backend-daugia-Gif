7f32f4d28c38827a53a5f537ce7525744f139d36
Phan Xuan Tan <PhanXuanTan4522@example.com>
2024-08-28 15:25:55 +0700

    3:25

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,18 +1,16 @@
-import { Sequelize } from 'sequelize';
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('dau_gia_Gif', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql' ,
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'mysql',
+  }
+);
 
-}); 
-
-let connectDB = async() => { 
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
-
-module.exports = connectDB;
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
